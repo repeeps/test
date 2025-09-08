a2cfee6e24ed464f20add5611120d51c2219a6e9
seoji <seoji1986@gmail.com>
2025-09-08 23:22:36 +0900

    connected MongoDB and post-service added

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 
-app.engine("handlebars", handlebars.engine());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongodbConnection = require("./configs/mongodb-connection");
+const postService = require("./services/post-service");
+
+app.engine(
+  "handlebars",
+  handlebars.create({
+    helpers: require("./configs/handlebars-helpers"),
+  }).engine,
+);
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
   res.render("home", { title: "테스트 게시판" });
+});
+
+app.post("/write", async (req, res) => {
+  const post = req.body;
+  const result = await postService.writePost(collection, post);
+
+  res.redirect(`/detail/${result.insertedId}`);
 });
 
 app.get("/write", (req, res) => {
@@ -17,4 +35,13 @@ app.get("/write", (req, res) => {
 app.get("/detail/:id", async (req, res) => {
   res.render("detail", { title: "테스트 게시판" });
 });
-app.listen(3000);
+
+let collection;
+app.listen(3000, async () => {
+  console.log("Server Started !");
+
+  const mongoClient = await mongodbConnection();
+
+  collection = mongoClient.db().collection("post");
+  console.log("MongoDB connected!");
+});
--- configs/mongodb-connection.js
+++ configs/mongodb-connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://jayseo:gKqTaQ4y3Q61yKQa@cluster0.uxjkbrv.mongodb.net/board?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://admin:1234@cluster0.x5ivvsh.mongodb.net/board?retryWrites=true&w=majority";
 
 module.exports = function (callback) {
     return MongoClient.connect(uri, callback);
