5126e3291f0b9016fe93c9994a8bf1ec35d39849
jay <jay.seo@evar.co.kr>
2025-09-09 09:42:20 +0900

    list & pagination

--- app.js
+++ app.js
@@ -17,8 +17,19 @@ app.engine(
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
-  res.render("home", { title: "테스트 게시판" });
+app.get("/", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const search = req.query.search || "";
+
+  try {
+    const [posts, paginator] = await postService.list(collection, page, search);
+
+    res.render("home", { title: "테스트 게시판", search, paginator, posts });
+  } catch (error) {
+    console.error(error);
+
+    res.render("home", { title: "테스트 게시판" });
+  }
 });
 
 app.post("/write", async (req, res) => {
--- services/post-service.js
+++ services/post-service.js
@@ -1,3 +1,5 @@
+const paginator = require("../utils/paginator");
+
 async function writePost(collection, post) {
   post.hits = 0;
   post.createdDt = new Date().toISOString();
@@ -5,6 +7,18 @@ async function writePost(collection, post) {
   return await collection.insertOne(post);
 }
 
+async function list(collection, page, search) {
+  const perPage = 10;
+  const query = { title: new RegExp(search, "i") };
+  const cursor = collection.find(query, { limit: perPage, skep: (page - 1) * perPage }).sort({ createdDt: -1 });
+  const totalCount = await collection.count(query);
+  const posts = await cursor.toArray();
+  const paginatorObj = paginator({ totalCount, page, perPage });
+
+  return [posts, paginatorObj];
+}
+
 module.exports = {
+  list,
   writePost,
 };
